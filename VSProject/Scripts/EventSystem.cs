c5eceb5ad8aa197e12952d38c2e5020e82310df1
MikVR7 <atmikes123@gmail.com>
2022-07-01 21:06:26 +0200

    update remove foreach statements

--- VSProject/Scripts/EventSystem.cs
+++ VSProject/Scripts/EventSystem.cs
@@ -84,16 +84,16 @@ namespace CodeEvents
     /// <summary>
     /// Event that takes 1 parameters.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class EventSystem<T> : AbstractEventSystem
+    /// <typeparam name="T0"></typeparam>
+    public class EventSystem<T0> : AbstractEventSystem
     {
-        private List<Action<T>> actions = new List<Action<T>>();
+        private List<Action<T0>> actions = new List<Action<T0>>();
 
         /// <summary>
         /// Add function to functions list - old method.
         /// </summary>
         /// <param name="action"></param>
-        public void AddListener(Action<T> action)
+        public void AddListener(Action<T0> action)
         {
             actions.Add(action);
         }
@@ -103,12 +103,12 @@ namespace CodeEvents
         /// Works in most cases way better!
         /// </summary>
         /// <param name="action"></param>
-        public void AddListenerSingle(Action<T> action)
+        public void AddListenerSingle(Action<T0> action)
         {
             if (!actions.Contains(action)) { actions.Add(action); }
         }
 
-        public void RemoveListener(Action<T> action)
+        public void RemoveListener(Action<T0> action)
         {
             if (actions.Contains(action))
             {
@@ -126,7 +126,7 @@ namespace CodeEvents
         /// exception if the list gets changed while iterating.
         /// </summary>
         /// <param name="param0"></param>
-        public void Invoke(T param0)
+        public void Invoke(T0 param0)
         {
             for (int i = 0; i < actions.Count; i++) { actions[i].Invoke(param0); }
         }
@@ -136,9 +136,9 @@ namespace CodeEvents
         /// and then the array gets iterated.
         /// </summary>
         /// <param name="param0"></param>
-        public void InvokeByArray(T param0)
+        public void InvokeByArray(T0 param0)
         {
-            Action<T>[] a = actions.ToArray();
+            Action<T0>[] a = actions.ToArray();
             for (int i = 0; i < a.Length; i++) { a[i].Invoke(param0); }
         }
 
@@ -156,17 +156,17 @@ namespace CodeEvents
     /// <summary>
     /// Event that takes 2 parameters.
     /// </summary>
+    /// <typeparam name="T0"></typeparam>
     /// <typeparam name="T1"></typeparam>
-    /// <typeparam name="T2"></typeparam>
-    public class EventSystem<T1, T2> : AbstractEventSystem
+    public class EventSystem<T0, T1> : AbstractEventSystem
     {
-        private List<Action<T1, T2>> actions = new List<Action<T1, T2>>();
+        private List<Action<T0, T1>> actions = new List<Action<T0, T1>>();
 
         /// <summary>
         /// Add function to functions list - old method.
         /// </summary>
         /// <param name="action"></param>
-        public void AddListener(Action<T1, T2> action)
+        public void AddListener(Action<T0, T1> action)
         {
             actions.Add(action);
         }
@@ -176,12 +176,12 @@ namespace CodeEvents
         /// Works in most cases way better!
         /// </summary>
         /// <param name="action"></param>
-        public void AddListenerSingle(Action<T1, T2> action)
+        public void AddListenerSingle(Action<T0, T1> action)
         {
             if (!actions.Contains(action)) { actions.Add(action); }
         }
 
-        public void RemoveListener(Action<T1, T2> action)
+        public void RemoveListener(Action<T0, T1> action)
         {
             if (actions.Contains(action))
             {
@@ -200,7 +200,7 @@ namespace CodeEvents
         /// </summary>
         /// <param name="param0"></param>
         /// <param name="param1"></param>
-        public void Invoke(T1 param0, T2 param1)
+        public void Invoke(T0 param0, T1 param1)
         {
             for (int i = 0; i < actions.Count; i++) { actions[i].Invoke(param0, param1); }
         }
@@ -209,12 +209,12 @@ namespace CodeEvents
         /// Invoke event inside array - slower but safer. List gets transformed to Array
         /// and then the array gets iterated.
         /// </summary>
+        /// <param name="param0"></param>
         /// <param name="param1"></param>
-        /// <param name="param2"></param>
-        public void InvokeByArray(T1 param1, T2 param2)
+        public void InvokeByArray(T0 param0, T1 param1)
         {
-            Action<T1, T2>[] a = actions.ToArray();
-            for (int i = 0; i < a.Length; i++) { a[i].Invoke(param1, param2); }
+            Action<T0, T1>[] a = actions.ToArray();
+            for (int i = 0; i < a.Length; i++) { a[i].Invoke(param0, param1); }
         }
 
         public bool HasListeners()
@@ -231,17 +231,17 @@ namespace CodeEvents
     /// <summary>
     /// Event that takes 3 parameters.
     /// </summary>
+    /// <typeparam name="T0"></typeparam>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
-    /// <typeparam name="T3"></typeparam>
-    public class EventSystem<T1, T2, T3> : AbstractEventSystem
+    public class EventSystem<T0, T1, T2> : AbstractEventSystem
     {
-        private List<Action<T1, T2, T3>> actions = new List<Action<T1, T2, T3>>();
+        private List<Action<T0, T1, T2>> actions = new List<Action<T0, T1, T2>>();
         /// <summary>
         /// Add function to functions list - old method.
         /// </summary>
         /// <param name="action"></param>
-        public void AddListener(Action<T1, T2, T3> action)
+        public void AddListener(Action<T0, T1, T2> action)
         {
             actions.Add(action);
         }
@@ -251,12 +251,12 @@ namespace CodeEvents
         /// Works in most cases way better!
         /// </summary>
         /// <param name="action"></param>
-        public void AddListenerSingle(Action<T1, T2, T3> action)
+        public void AddListenerSingle(Action<T0, T1, T2> action)
         {
             if (!actions.Contains(action)) { actions.Add(action); }
         }
 
-        public void RemoveListener(Action<T1, T2, T3> action)
+        public void RemoveListener(Action<T0, T1, T2> action)
         {
             if (actions.Contains(action))
             {
@@ -276,7 +276,7 @@ namespace CodeEvents
         /// <param name="param0"></param>
         /// <param name="param1"></param>
         /// <param name="param2"></param>
-        public void Invoke(T1 param0, T2 param1, T3 param2)
+        public void Invoke(T0 param0, T1 param1, T2 param2)
         {
             for (int i = 0; i < actions.Count; i++) { actions[i].Invoke(param0, param1, param2); }
         }
@@ -288,10 +288,10 @@ namespace CodeEvents
         /// <param name="param1"></param>
         /// <param name="param2"></param>
         /// <param name="param3"></param>
-        public void InvokeByArray(T1 param1, T2 param2, T3 param3)
+        public void InvokeByArray(T0 param0, T1 param1, T2 param2)
         {
-            Action<T1, T2, T3>[] a = actions.ToArray();
-            for (int i = 0; i < a.Length; i++) { a[i].Invoke(param1, param2, param3); }
+            Action<T0, T1, T2>[] a = actions.ToArray();
+            for (int i = 0; i < a.Length; i++) { a[i].Invoke(param0, param1, param2); }
         }
 
         public bool HasListeners()
@@ -308,18 +308,18 @@ namespace CodeEvents
     /// <summary>
     /// Event that takes 4 parameters.
     /// </summary>
+    /// <typeparam name="T0"></typeparam>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
     /// <typeparam name="T3"></typeparam>
-    /// <typeparam name="T4"></typeparam>
-    public class EventSystem<T1, T2, T3, T4> : AbstractEventSystem
+    public class EventSystem<T0, T1, T2, T3> : AbstractEventSystem
     {
-        private List<Action<T1, T2, T3, T4>> actions = new List<Action<T1, T2, T3, T4>>();
+        private List<Action<T0, T1, T2, T3>> actions = new List<Action<T0, T1, T2, T3>>();
         /// <summary>
         /// Add function to functions list - old method.
         /// </summary>
         /// <param name="action"></param>
-        public void AddListener(Action<T1, T2, T3, T4> action)
+        public void AddListener(Action<T0, T1, T2, T3> action)
         {
             actions.Add(action);
         }
@@ -329,12 +329,12 @@ namespace CodeEvents
         /// Works in most cases way better!
         /// </summary>
         /// <param name="action"></param>
-        public void AddListenerSingle(Action<T1, T2, T3, T4> action)
+        public void AddListenerSingle(Action<T0, T1, T2, T3> action)
         {
             if (!actions.Contains(action)) { actions.Add(action); }
         }
 
-        public void RemoveListener(Action<T1, T2, T3, T4> action)
+        public void RemoveListener(Action<T0, T1, T2, T3> action)
         {
             if (actions.Contains(action))
             {
@@ -355,7 +355,7 @@ namespace CodeEvents
         /// <param name="param1"></param>
         /// <param name="param2"></param>
         /// <param name="param3"></param>
-        public void Invoke(T1 param0, T2 param1, T3 param2, T4 param3)
+        public void Invoke(T0 param0, T1 param1, T2 param2, T3 param3)
         {
             for (int i = 0; i < actions.Count; i++) { actions[i].Invoke(param0, param1, param2, param3); }
         }
@@ -364,14 +364,14 @@ namespace CodeEvents
         /// Invoke event inside array - slower but safer. List gets transformed to Array
         /// and then the array gets iterated.
         /// </summary>
+        /// <param name="param0"></param>
         /// <param name="param1"></param>
         /// <param name="param2"></param>
         /// <param name="param3"></param>
-        /// <param name="param4"></param>
-        public void InvokeByArray(T1 param1, T2 param2, T3 param3, T4 param4)
+        public void InvokeByArray(T0 param0, T1 param1, T2 param2, T3 param3)
         {
-            Action<T1, T2, T3, T4>[] a = actions.ToArray();
-            for (int i = 0; i < a.Length; i++) { a[i].Invoke(param1, param2, param3, param4); }
+            Action<T0, T1, T2, T3>[] a = actions.ToArray();
+            for (int i = 0; i < a.Length; i++) { a[i].Invoke(param0, param1, param2, param3); }
         }
 
         public bool HasListeners()
@@ -388,19 +388,19 @@ namespace CodeEvents
     /// <summary>
     /// Event that takes 5 parameters.
     /// </summary>
+    /// <typeparam name="T0"></typeparam>
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
     /// <typeparam name="T3"></typeparam>
     /// <typeparam name="T4"></typeparam>
-    /// <typeparam name="T5"></typeparam>
-    public class EventSystem<T1, T2, T3, T4, T5> : AbstractEventSystem
+    public class EventSystem<T0, T1, T2, T3, T4> : AbstractEventSystem
     {
-        private List<Action<T1, T2, T3, T4, T5>> actions = new List<Action<T1, T2, T3, T4, T5>>();
+        private List<Action<T0, T1, T2, T3, T4>> actions = new List<Action<T0, T1, T2, T3, T4>>();
         /// <summary>
         /// Add function to functions list - old method.
         /// </summary>
         /// <param name="action"></param>
-        public void AddListener(Action<T1, T2, T3, T4, T5> action)
+        public void AddListener(Action<T0, T1, T2, T3, T4> action)
         {
             actions.Add(action);
         }
@@ -410,12 +410,12 @@ namespace CodeEvents
         /// Works in most cases way better!
         /// </summary>
         /// <param name="action"></param>
-        public void AddListenerSingle(Action<T1, T2, T3, T4, T5> action)
+        public void AddListenerSingle(Action<T0, T1, T2, T3, T4> action)
         {
             if (!actions.Contains(action)) { actions.Add(action); }
         }
 
-        public void RemoveListener(Action<T1, T2, T3, T4, T5> action)
+        public void RemoveListener(Action<T0, T1, T2, T3, T4> action)
         {
             if (actions.Contains(action))
             {
@@ -437,7 +437,7 @@ namespace CodeEvents
         /// <param name="param2"></param>
         /// <param name="param3"></param>
         /// <param name="param4"></param>
-        public void Invoke(T1 param0, T2 param1, T3 param2, T4 param3, T5 param4)
+        public void Invoke(T0 param0, T1 param1, T2 param2, T3 param3, T4 param4)
         {
             for (int i = 0; i < actions.Count; i++) { actions[i].Invoke(param0, param1, param2, param3, param4); }
         }
@@ -446,15 +446,15 @@ namespace CodeEvents
         /// Invoke event inside array - slower but safer. List gets transformed to Array
         /// and then the array gets iterated.
         /// </summary>
+        /// <param name="param0"></param>
         /// <param name="param1"></param>
         /// <param name="param2"></param>
         /// <param name="param3"></param>
         /// <param name="param4"></param>
-        /// <param name="param5"></param>
-        public void InvokeByArray(T1 param1, T2 param2, T3 param3, T4 param4, T5 param5)
+        public void InvokeByArray(T0 param0, T1 param1, T2 param2, T3 param3, T4 param4)
         {
-            Action<T1, T2, T3, T4, T5>[] a = actions.ToArray();
-            for (int i = 0; i < a.Length; i++) { a[i].Invoke(param1, param2, param3, param4, param5); }
+            Action<T0, T1, T2, T3, T4>[] a = actions.ToArray();
+            for (int i = 0; i < a.Length; i++) { a[i].Invoke(param0, param1, param2, param3, param4); }
         }
 
         public bool HasListeners()
